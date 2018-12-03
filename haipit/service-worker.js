61e9a75fedd5dc9b1ae14cd6574111f6246efde3
z80 <muzo0@yandex.ru>
2018-12-03 21:32:47 +0300

    fix path haipit

--- haipit/service-worker.js
+++ haipit/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fdbde7d10d2ec4d329f2fdae4c595af9.js"
+  "./precache-manifest.6b66dbb0d804e935d46dc9baf58f47a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
