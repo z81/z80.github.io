64fb9f1ccf911e056d9d58812af4cd3b148133f1
z80 <muzo0@yandex.ru>
2018-12-03 22:04:39 +0300

    upd

---
+++ haipit/precache-manifest.b47915ed11bdf957475b6f02440ec399.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "57534d39e6776d7b3b8a",
+    "url": "./static/js/main.57534d39.chunk.js"
+  },
+  {
+    "revision": "abc8d1df0b4aa563cc1c",
+    "url": "./static/js/1.abc8d1df.chunk.js"
+  },
+  {
+    "revision": "57534d39e6776d7b3b8a",
+    "url": "./static/css/main.9e582c86.chunk.css"
+  },
+  {
+    "revision": "8afeaab627d2507d9816bd932d02fb20",
+    "url": "./index.html"
+  }
+];
