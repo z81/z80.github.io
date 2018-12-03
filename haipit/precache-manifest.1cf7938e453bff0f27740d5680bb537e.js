e3cbf1801e54450a02252ce72c35bf0528731ce0
z80 <muzo0@yandex.ru>
2018-12-03 21:36:01 +0300

    fix path haipit

---
+++ haipit/precache-manifest.1cf7938e453bff0f27740d5680bb537e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0e8de4d668e92434b34d",
+    "url": "./static/js/main.0e8de4d6.chunk.js"
+  },
+  {
+    "revision": "abc8d1df0b4aa563cc1c",
+    "url": "./static/js/1.abc8d1df.chunk.js"
+  },
+  {
+    "revision": "0e8de4d668e92434b34d",
+    "url": "./static/css/main.9e582c86.chunk.css"
+  },
+  {
+    "revision": "62c1e2b7d1d694678d46994f384985ec",
+    "url": "./index.html"
+  }
+];
